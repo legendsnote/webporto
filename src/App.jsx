894c042e87d6e9ee4befb5ae13a3a46461c1cdb0
Geraldo PL <geraldo.p.lutan@gmail.com>
2025-10-14 19:07:33 +0700

    Update experience years

--- src/App.jsx
+++ src/App.jsx
@@ -65,9 +65,9 @@ function App() {
               </div>
               <div>
                 <h1 className="text-4xl mb-1  ">
-                  2<span className=" ">+</span> 
+                  1<span className=" ">+</span> 
                 </h1>
-                <p>Year Programming Experience</p>
+                <p>Year Working Experience</p>
               </div>
             </div>
           </div>
